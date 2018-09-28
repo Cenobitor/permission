ed2cb65a8a67565a813a3abbc16aff40d29bab85
gdw <1037345628@qq.com>
2018-09-28 23:28:13 +0800

    项目基本框架

---
+++ src/main/java/com/cenobitor/dao/SysLogMapper.java
@@ -0,0 +1,20 @@
+package com.cenobitor.dao;
+
+import com.cenobitor.model.SysLog;
+import com.cenobitor.model.SysLogWithBLOBs;
+
+public interface SysLogMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(SysLogWithBLOBs record);
+
+    int insertSelective(SysLogWithBLOBs record);
+
+    SysLogWithBLOBs selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(SysLogWithBLOBs record);
+
+    int updateByPrimaryKeyWithBLOBs(SysLogWithBLOBs record);
+
+    int updateByPrimaryKey(SysLog record);
+}
